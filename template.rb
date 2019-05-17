a122f6b85f0bb54748a6100ef7c8b6136d7fcaf4
Scott Goyette <scott.goyette@rubypythmore.com>
2019-05-17 16:05:33 -0400

    added .env file addition

--- template.rb
+++ template.rb
@@ -15,8 +15,10 @@ generate(:scaffold, 'user', 'first_name', 'last_name')
 rails_command('generate devise:install')
 rails_command('generate devise User')
 
-environment 'config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }', env: 'development'
-environment 'config.action_mailer.default_url_options = { host: 'https://example.com', port: 3000 }', env: 'production'
+environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
+environment "config.action_mailer.default_url_options = { host: 'https://example.com', port: 3000 }", env: 'production'
+
+file './.env'
 
 # file 'config/initializer/rubypythmore.rb', <<-CODE
 initializer 'rubypythmore.rb', <<-CODE
