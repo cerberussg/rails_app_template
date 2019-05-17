671f4240702913568a76a17cc5c2ff49847b36cd
Scott Goyette <scott.goyette@rubypythmore.com>
2019-05-17 15:58:16 -0400

    fixed quotes

--- template.rb
+++ template.rb
@@ -2,34 +2,34 @@
 # rails new myapp -m https://raw.github.com/cerberussg/rails_app_template/master/template.rb -> utilizing the github repo
 # rails new myapp -> via ~/.railsrc file
 
-gem "devise"
-gem "dotenv-rails"
+gem 'devise'
+gem 'dotenv-rails'
 gem_group :development, :test do
-  gem "rspec-rails"
+  gem 'rspec-rails'
 end
 
-run "bundle install"
+run 'bundle install'
 
-generate(:scaffold, "user", "first_name", "last_name")
+generate(:scaffold, 'user', 'first_name', 'last_name')
 
-rails_command("generate devise:install")
-rails_command("generate devise User")
+rails_command('generate devise:install')
+rails_command('generate devise User')
 
-environment "config.action_mailer.default_url_options = { host: "localhost", port: 3000 }", env: "development"
-environment "config.action_mailer.default_url_options = { host: "https://example.com", port: 3000 }", env: "production"
+environment 'config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }', env: 'development'
+environment 'config.action_mailer.default_url_options = { host: 'https://example.com', port: 3000 }', env: 'production'
 
-file "./.env"
+file './.env'
 
-# file "config/initializer/rubypythmore.rb", <<-CODE
-initializer "rubypythmore.rb", <<-CODE
+# file 'config/initializer/rubypythmore.rb', <<-CODE
+initializer 'rubypythmore.rb', <<-CODE
   # something 1
   # something 2
   # something 3
 CODE
 
-if yes?("Create another initializer?")
-  init_name = ask("What do you want to call the initializer?")
-  init_name << ".rb" unless init_name.index(".rb")
+if yes?('Create another initializer?')
+  init_name = ask('What do you want to call the initializer?')
+  init_name << '.rb' unless init_name.index('.rb')
   initializer init_name, <<-CODE
     # something
   CODE
@@ -37,10 +37,10 @@ if yes?("Create another initializer?")
   # yes
 end
 
-route "root to: users#index"
+route 'root to: users#index'
 
 after_bundle do
   git :init
-  git add: "."
-  git commit: %( -m "Initial commit")
+  git add: '.'
+  git commit: %( -m 'Initial commit')
 end
